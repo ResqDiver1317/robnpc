cd1dc01cd42b5e3e2f62e4e15f82a52195324bb9
ResqDiver1317 <jasont@ladueweb.com>
2022-02-23 01:22:13 -0600

    Update and rename __resource.lua to fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,6 +4,7 @@ game { 'gta5' }
 author 'Resq#1317'
 description 'Rob NPC script made to work with currency system and framework from Andyyy7666'
 --Credit for Original version to c0deina - https://github.com/c0deina/esx_robnpc
+--Alert function modified from SpikE-Odets's port of c0deina's script. https://github.com/SpikE-Odets/esx_mugging
 --Modified by Resq#1317 to work with Andyyy7666's Money Script and/or Framework.
 
 server_scripts {
