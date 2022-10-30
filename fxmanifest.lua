8f868d48b0f6930c6964689d3aaf29905ce7860f
Marshular <78507312+marshular@users.noreply.github.com>
2022-10-30 10:27:29 -0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,10 +2,10 @@ fx_version 'cerulean'
 
 game { 'gta5' }
 author 'Resq#1317'
-description 'Rob NPC script made to work with currency system and framework from Andyyy7666'
+description 'Rob NPC script made to work with NDCore'
 --Credit for Original version to c0deina - https://github.com/c0deina/esx_robnpc
 --Alert function modified from SpikE-Odets's port of c0deina's script. https://github.com/SpikE-Odets/esx_mugging
---Modified by Resq#1317 to work with Andyyy7666's Money Script and/or Framework.
+--Modified by Resq#1317 to work with NDCore.
 
 server_scripts {
      'config.lua',
