7c180bbb22cb3db1790dc735080bb5b99bbfc842
Marshular <78507312+marshular@users.noreply.github.com>
2022-10-29 21:56:33 -0700

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,22 +1,5 @@
 NDCore = exports["ND_Core"]:GetCoreObject()
 
--- Get player identifier function (This will not change the identifier all the time)
-function GetPlayerIdentifierFromType(type, source) -- Credits: xander1998, Post: https://forum.cfx.re/t/solved-is-there-a-better-way-to-get-lic-steam-and-ip-than-getplayeridentifiers/236243/2?u=andyyy7666
-	local identifiers = {}
-	local identifierCount = GetNumPlayerIdentifiers(source)
-
-	for a = 0, identifierCount do
-		table.insert(identifiers, GetPlayerIdentifier(source, a))
-	end
-
-	for b = 1, #identifiers do
-		if string.find(identifiers[b], type, 1) then
-			return identifiers[b]
-		end
-	end
-	return nil
-end
-
 AddEventHandler("robnpc:getcash")
 RegisterServerEvent('robnpc:getcash', function()
     local player = source
