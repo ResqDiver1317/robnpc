a1ee701b2a771b7daad8811b5bf641b0350aab5a
Marshular <marshy0909@gmail.com>
2022-11-09 19:10:16 -0800

    Rework
    Just added some final touches

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,19 +1,13 @@
 fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
 
-game { 'gta5' }
-author 'Resq#1317'
-description 'Rob NPC script made to work with NDCore'
---Credit for Original version to c0deina - https://github.com/c0deina/esx_robnpc
---Alert function modified from SpikE-Odets's port of c0deina's script. https://github.com/SpikE-Odets/esx_mugging
---Modified by Resq#1317 to work with NDCore.
-
-server_scripts {
-     'config.lua',
-     'server.lua'
-}
-
-client_scripts {
+shared_scripts {
     'config.lua',
-    'client.lua'
+    '@ox_lib/init.lua'
 }
+
+server_script 'source/server/main.lua'
+
+client_script 'source/client/main.lua'
 
