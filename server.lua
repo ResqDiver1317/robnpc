0d728b05a0f8dbd9b4f8a1d0bd3213b546aa2f32
ResqDiver1317 <jasont@ladueweb.com>
2024-02-19 01:10:00 -0600

    Update server.lua
    Removed functionality for standalone currency. Updated event for receiving the cash with ND_Core.
    
    Updated the server event for the 911 trigger. If your 911 system uses server side exports/events to activate, you can edit the event here to fit your needs. If your 911 system uses client side events, leave this alone and edit the 911 event in the client.lua.

--- server.lua
+++ server.lua
@@ -1,58 +1,24 @@
--- Get player identifier function (This will not change the identifier all the time)
-function GetPlayerIdentifierFromType(type, source) -- Credits: xander1998, Post: https://forum.cfx.re/t/solved-is-there-a-better-way-to-get-lic-steam-and-ip-than-getplayeridentifiers/236243/2?u=andyyy7666
-	local identifiers = {}
-	local identifierCount = GetNumPlayerIdentifiers(source)
 
-	for a = 0, identifierCount do
-		table.insert(identifiers, GetPlayerIdentifier(source, a))
-	end
-
-	for b = 1, #identifiers do
-		if string.find(identifiers[b], type, 1) then
-			return identifiers[b]
-		end
-	end
-	return nil
-end
-
-AddEventHandler("robnpc:getcash")
+AddEventHandler('robnpc:getcash')
 RegisterServerEvent('robnpc:getcash', function()
-    local player = source
-    local amount = (math.random(Config.MinMoney,Config.MaxMoney))
-        if Config.Framework then
-            exports.oxmysql:query("UPDATE characters SET cash = cash + ? WHERE license = ?", {amount, GetPlayerIdentifierFromType("license", player)})
-            TriggerClientEvent("updateMoney", player, amount, "cash")
-        else
-            exports.oxmysql:query("UPDATE money SET cash = cash + ? WHERE license = ?", {amount, GetPlayerIdentifierFromType("license", player)})
-            TriggerClientEvent("updateMoney", player, amount, "cash")
-        end
-        TriggerClientEvent("chat:addMessage", player, {
-        color = {0, 255, 100},
-        args = {Config.StealAmountTitle, Config.StealAmountMessage ..amount.. " "}
-        })
-end)
-
-RegisterServerEvent('robnpc:muggingAlert')
-AddEventHandler('robnpc:muggingAlert', function(street1, street2, reportcoords)
-    TriggerClientEvent("chat:addMessage", -1, {
-        color = {255, 100, 100},
-        args = {Config.AlertTitle, Config.AlertMessage..street1.." & "..street2, reportcoords}
-        })
-    --TriggerClientEvent("muggingNotify", -1, "~r~Reported Mugging by ~r~near~w~ "..street1.." and "..street2, coords)
+    local player = NDCore.getPlayer(source)
+    local amount = stealamount
+    if amount > 0 then
+        local success = player.addMoney("cash", amount, "Stolen Money from Robbery")
+        print(success)
+    else
+        TriggerClientEvent('chatMessage', player, '', {100,200,100}, 'You did not steal any money!')
+    end
 end)
 
 
-RegisterServerEvent('robnpc:muggingAlertS1')
-AddEventHandler('robnpc:muggingAlertS1', function(street1, reportcoords)
-    TriggerClientEvent("chat:addMessage", -1, {
-        color = {255, 100, 100},
-        args = {Config.AlertTitle, Config.AlertMessage ..street1, reportcoords}
-        })
-    --TriggerClientEvent("muggingNotify", -1, "~r~Reported Mugging by ~r~near~w~ "..street1, coords)
+--If your 911 system uses Server Side exports or Events to trigger, edit this section below to trigger it. 
+--If your 911 system uses Client Side exports or Events to trigger, leave this alone and edit the 'robnpc:911Client' event in the client.lua file.
+--By default, this script is configured to function with Night's MDT system. 
+--Support is not provided related to configuration of other MDT/CAD/911 scripts.
+AddEventHandler('robnpc:911')
+RegisterServerEvent('robnpc:911', function(source)
+    local ped = source
+    TriggerClientEvent("robnpc:911Client", ped) --For Server Side 911 triggers, replace this line with your event/export. 
 end)
 
-
-RegisterServerEvent('robnpc:muggingPos')
-AddEventHandler('robnpc:muggingPos', function(gx, gy, gz)
-	TriggerClientEvent('robnpc:muggingPos', -1, gx, gy, gz)
-end)
