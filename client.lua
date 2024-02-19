7d9a1194c7bdf9357ae6400c6b33a06e8867c9c9
ResqDiver1317 <jasont@ladueweb.com>
2024-02-19 01:06:32 -0600

    Update client.lua
    Updated to version 2.00.
    
    Updated client side functions for notifying 911. Defaults to send calls to Night's nightshift MDT system. Specific section for servers to edit the 911 function to fit the needs of their 911 system if triggerred client side. (This event is triggerred by a Server Event so if your 911 uses server side exports/events to trigger, that can be done in the server.lua.
    
    Added functionality to allow for random length of robberies vs fixed length using min/max times set in config.

--- client.lua
+++ client.lua
@@ -55,23 +55,9 @@ function robNpc(targetPed)
         end
         --start report chat message data
         if Config.Notify then
-            local plyPos = GetEntityCoords(GetPlayerPed(-1), true)
-            local s1, s2 = Citizen.InvokeNative( 0x2EB41072B4C1E4C0, plyPos.x, plyPos.y, plyPos.z, Citizen.PointerValueInt(), Citizen.PointerValueInt() )
-                local street1 = GetStreetNameFromHashKey(s1)
-                local street2 = GetStreetNameFromHashKey(s2)
-            reportcoords = {
-                x = plyPos.x,
-                y = plyPos.y,
-                z = plyPos.z,
-            }
             local alertmath = (math.random(1,100))
             if alertmath <= Config.AlertChance then
-                TriggerServerEvent('robnpc:muggingPos', plyPos.x, plyPos.y, plyPos.z)
-                if s2 == 0 then
-                    TriggerServerEvent('robnpc:muggingAlertS1', street1, reportcoords)
-                elseif s2 ~= 0 then
-                    TriggerServerEvent('robnpc:muggingAlert', street1, street2, reportcoords)
-                end
+                TriggerServerEvent("robnpc:911")
             end
         end
         --end report chat message data
@@ -82,8 +68,9 @@ function robNpc(targetPed)
         TaskPlayAnim(targetPed, dict, 'cower_loop', 8.0, -8, 15000.0, 49, 0, 0, 0, 0)
         --TaskPlayAnim(targetPed, dict, 'handsup_standing_base', 8.0, -8, .01, 49, 0, 0, 0, 0)
         ShowInfo(Config.Started)
-
-        Citizen.Wait(Config.RobAnimationSeconds * 1000)
+        
+        local robtime = math.random(Config.RobTimeMinSeconds,Config.RobTimeMaxSeconds)
+        Citizen.Wait(robtime * 1000)
         randomact = math.random(1,10)
             if randomact > 6 then
                 PlayAmbientSpeech1(targetPed, "GENERIC_INSULT_HIGH", "SPEECH_PARAMS_FORCE_NORMAL_CLEAR")
@@ -109,21 +96,13 @@ function robNpc(targetPed)
         end)
 end
 
-RegisterNetEvent('robnpc:muggingPos')
-AddEventHandler('robnpc:muggingPos', function(tx, ty, tz)
-		local transT = 1000
-		local Blip = AddBlipForCoord(tx, ty, tz)
-		SetBlipSprite(Blip,  10)
-		SetBlipColour(Blip,  1)
-		SetBlipAlpha(Blip,  transT)
-		SetBlipAsShortRange(Blip,  false)
-		while transT ~= 0 do
-			Wait(100)
-			transT = transT - 1
-			SetBlipAlpha(Blip,  transT)
-			if transT == 0 then
-				SetBlipSprite(Blip,  2)
-				return
-			end
-	end
+--If your 911 system uses client side exports or events to trigger, edit the section below to suit your system's needs. 
+--By default, this script is configured to function with Night's MDT system. 
+--Support is not provided related to configuration of other MDT/CAD/911 scripts.
+RegisterNetEvent('robnpc:911Client')
+AddEventHandler('robnpc:911Client', function(ped)
+    local ped2 = ped
+    local coords = GetEntityCoords(ped2)
+    local alarmmsg = "ANON REPORT: Armed robbery of local citizen!"
+    exports.night_shifts:TriggerAlarm(true --[[ bool ]], true --[[ bool ]], false --[[ bool ]], false --[[ bool ]], false --[[ bool ]], alarmmsg --[[ string ]], coords --[[ Vector3 ]])
 end)
