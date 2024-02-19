63e3f6097bffd559c44336b0703c98a5bc0aff87
ResqDiver1317 <jasont@ladueweb.com>
2024-02-19 01:01:38 -0600

    Update fxmanifest.lua
    Updating to version 2.0.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,14 +6,24 @@ description 'Rob NPC script made to work with currency system and framework from
 --Credit for Original version to c0deina - https://github.com/c0deina/esx_robnpc
 --Alert function modified from SpikE-Odets's port of c0deina's script. https://github.com/SpikE-Odets/esx_mugging
 --Modified by Resq#1317 to work with Andyyy7666's Money Script and/or Framework.
+lua54 'yes'
+
+Version '2.00'
 
 server_scripts {
-     'config.lua',
      'server.lua'
 }
 
 client_scripts {
-    'config.lua',
     'client.lua'
 }
 
+shared_scripts {
+    'config.lua',
+    '@ox_lib/init.lua',
+    "@ND_Core/init.lua",
+}
+
+ox_libs {
+    'interface',
+}
