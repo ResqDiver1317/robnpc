c3a0dd1f28230ee19265cb9b6a9010d69c1cb730
ResqDiver1317 <jasont@ladueweb.com>
2024-02-19 01:03:29 -0600

    Update config.lua
    Update to version 2.0
    Removed option to select between standalone currency and ND_Core. Script now works exclusively on ND_Core's currency.
    Added config options for min/max robbery times to allow for random lenght of robbery instead of fixed length.

--- config.lua
+++ config.lua
@@ -1,18 +1,18 @@
 Config = {}
-Config.Framework = false --If using Andyyy's Framework, set to true. If just using Andyyy's currency system and NOT the full framework, set to false.
 
 Config.ShouldWaitBetweenRobbing = true --Setting to true will cause a random time within the range defined below to be required before a player can rob another NPC. Setting to false will allow the player to immediately rob another NPC.
 Config.MinWaitSeconds = 60 --Minimum wait time in seconds.
 Config.MaxWaitSeconds = 180  --Maximum wait time in seconds.
 
 Config.RobDistance = 3 --Maximum distance from the NPC to rob them. If too far away, players will receive a message saying they are too far away.
-Config.RobAnimationSeconds = 60 --Time in seconds needed for the robbery to complete.
+Config.RobTimeMinSeconds = 60 --Minimum time in seconds needed for the robbery to complete.
+Config.RobTimeMaxSeconds = 120 --Maximum time in seconds needed for the robbery to complete.
 
 Config.MinMoney = 1 --Minimum amount of money to get from a robbery
 Config.MaxMoney = 200 --Maximum amount of money to get from a robbery.
 
 Config.Notify = true --If you want to have a chance of an alert being sent for the mugging, set as true. If not, then set as false.
-Config.AlertChance = 60 --Percent of notification chance. Default of 60 means there is a 60% chance of an alert being sent for your robbery.
+Config.AlertChance = 60 --Percent of notification chance. Default of 60 means there is a 60(ish)% chance of an alert being sent for your robbery.
 
 
 --NOTICE ABOUT THE LANGUAGE CONFIG!! At the moment, the settings below will not actually affect the messages in game. This is a coming feature that should be released in the next day or 2 but at the moment, all the text for messages and notifications is set manually in the server and client lua files.
